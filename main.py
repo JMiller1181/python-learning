37840bd376ffbb0267c6204e0b4eb32a30855f39
Jacob Miller <millerjr1181@gmail.com>
2023-07-06 12:09:13 -0400

    initial commit with some code already present

---
+++ main.py
@@ -0,0 +1,68 @@
+# print("Hello world!")
+import math
+import time
+
+name = "Jacob"
+age = 27
+age += 1
+# print(name + " is " + str(age))
+height = 5.5
+# print("He is " + str(height) + " feet tall")
+last_name, weight, is_adult = "Miller", 165.3, True
+full_name = name + " " + last_name
+# print(last_name)
+# print(weight)
+# print(is_adult)
+# print(last_name.find("l"))
+# print(len(name))
+# print(name.lower())
+# print(name.upper())
+# print(last_name.isdigit())
+# print(name.isalpha())
+# print(last_name.count("l"))
+# print(name*3)
+# string_input = input("Please enter a value: ")
+# int_input = int(input("Please enter a value: "))
+# print(type(string_input))
+# print(type(int_input))
+# print(round(weight))
+# print(math.ceil(weight))
+# print(max(height, age, weight))
+# print(math.pow(age, 2))
+# nickname = full_name[0:3:2]
+# step_name = full_name[::2]
+# reverse_name = full_name[::-1]
+# print(nickname)
+# print(step_name)
+# print(reverse_name)
+# search = "http://google.com"
+# watch = "http://youtube.com"
+# slice = slice(7, -4)
+# print(search[slice])
+# print(watch[slice])
+# bad_input = False
+# user_age = int(input("Please enter your age: "))
+# if user_age >= 18:
+#     is_adult = True
+# elif 18 > user_age > 0:
+#     is_adult = False
+# else:
+#     bad_input = True
+# if is_adult == True and bad_input == False:
+#     print("You are an adult")
+# elif is_adult == False and bad_input == False:
+#     print("You are not an adult")
+# else:
+#     print("Invalid Age")
+# new_name = None
+# while not new_name:
+#     new_name = input("Please enter your name:")
+#     print(new_name)
+# for i in range(age):
+#     print(i+1)
+# for i in full_name:
+#     print(i)
+# for seconds in range(10, 0, -1):
+#     print(seconds)
+#     time.sleep(1)
+# print("Count down over!")
